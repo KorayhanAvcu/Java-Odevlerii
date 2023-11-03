05f654a9954aaa21620f2771a4bec001a1c23b2a
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-11-03 20:06:56 +0300

    Add files via upload

---
+++ Java Odev Palindromik Kelimeler/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+
+    static boolean isPalindrome(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir kelime giriniz : ");
+        String str = inp.nextLine();
+        System.out.println(isPalindrome(str));
+    }
+}
